76cf83c642f09b30aa396ad826f6c9e9c6203923
LT-058-Darren-Leo <lt-058-darren-leo@LT-058-Junie-Lek.local>
2024-01-16 18:26:04 +0800

    fix(part3): remove primitive fields in Life type resolver

--- src/server/schema/resolvers/types/Life.ts
+++ src/server/schema/resolvers/types/Life.ts
@@ -2,13 +2,7 @@ import { GraphQLLifeResolvers } from '../definitions';
 
 const LifeGraphQL: GraphQLLifeResolvers = {
     id: root => root._id,
-    firstName: root => root.firstName,
-    lastName: root => root.lastName,
     fullName: root => root.firstName + " " + root.lastName,
-    title: root => root.title,
-    description: root => root.description,
-    birthday: root => root.birthday,
-    hobbies: root => root.hobbies
 };
 
 export default LifeGraphQL;
